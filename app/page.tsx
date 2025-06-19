fdfe5d36c2108b1390b9dae25042103e4ef57fe0
Diego Rubalcava <diegoarlechuga@gmail.com>
2025-06-19 15:28:34 -0600

    added some colors and changed screen structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,81 @@
+"use client";
+
 import Image from "next/image";
 import TextField from "@mui/material/TextField";
 import IconButton from "@mui/material/IconButton";
 import SendIcon from "@mui/icons-material/Send";
-import { colors, Tooltip } from "@mui/material";
+import {
+    colors,
+    styled,
+    Tooltip,
+    tooltipClasses,
+    TooltipProps,
+} from "@mui/material";
+import { useState } from "react";
+import { ArrowUpward } from "@mui/icons-material";
+
+const BootstrapTooltip = styled(({ className, ...props }: TooltipProps) => (
+    <Tooltip {...props} arrow classes={{ popper: className }} />
+))(({ theme }) => ({
+    [`& .${tooltipClasses.arrow}`]: {
+        color: theme.palette.common.black,
+    },
+    [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: theme.palette.common.black,
+    },
+}));
 
 export default function Home() {
+    const [prompt, setPrompt] = useState("");
+
+    const onDisableSend = (): boolean => {
+        if (prompt.trim() === "") {
+            return true;
+        }
+        return false;
+    };
+
     return (
         <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center h-screen">
-            <h1 className="text-3xl text-center pb-4">Chatbot</h1>
-            <div className="h-8/12 w-8/12 bg-gray-500/20 border-2 border-gray-600/30 rounded-lg shadow-md mb-8 flex flex-col p-4 overflow-y-auto">
-                <div className="flex mb-3">
-                    <div className="bg-blue-300 rounded-lg py-2 px-3 max-w-[80%]">
-                        <p className="text-sm text-gray-800">
-                            Hola!. Cómo estás?.
-                        </p>
-                        <p className="text-xs text-gray-500 text-right mt-1">
-                            Chat
-                        </p>
-                    </div>
-                </div>
-
-                <div className="flex mb-3 justify-end">
-                    <div className="bg-blue-300 rounded-lg py-2 px-3 max-w-[80%]">
-                        <p className="text-sm text-gray-800">
-                            ¡Muy bien, gracias!
-                        </p>
-                        <p className="text-xs text-gray-500 text-right mt-1">
-                            Diego
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="w-full max-w-8/12">
-                <div className="flex gap-2">
-                    <TextField
-                        type="text"
-                        className="flex-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-white focus:border-white w-full"
-                        placeholder="Escribe un mensaje..."
-                        variant="standard"
-                        sx={{
-                            "& .MuiInputBase-input": {
-                                // Estilo de la entrada
-                                color: "white", // Color de la fuente
-                            },
-                        }}
-                    />
-                    <Tooltip title="Send" placement="right" arrow>
-                        <IconButton
-                            aria-label="send"
-                            style={{ color: "white" }}
+            <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center h-screen">
+                <h1 className="text-4xl text-center pb-4">Chatbot</h1>
+                <div
+                    className="h-2/12 w-6/12 messagesBackground border-2 
+                border-gray-600/30 rounded-xl shadow-md mb-8 flex flex-col p-4 overflow-y-auto"
+                >
+                    <div className="grid grid-cols-12">
+                        <input
+                            type="text"
+                            placeholder="How can I help you today?"
+                            className="col-span-11 focus:outline-none mt-4"
+                            onChange={(e) => setPrompt(e.target.value)}
+                            value={prompt}
+                        />
+                        <BootstrapTooltip
+                            title={prompt === "" ? "Message empty." : ""}
+                            placement="right"
                         >
-                            <SendIcon fontSize="inherit" />
-                        </IconButton>
-                    </Tooltip>
+                            <div
+                                className={
+                                    prompt === ""
+                                        ? "rounded-full bg-gray-300 mx-auto mt-4"
+                                        : "rounded-full bg-blue-500 mx-auto mt-4"
+                                }
+                            >
+                                <IconButton>
+                                    <ArrowUpward
+                                        fontSize="medium"
+                                        sx={{
+                                            color:
+                                                prompt === ""
+                                                    ? "disabled"
+                                                    : "white",
+                                        }}
+                                    />
+                                </IconButton>
+                            </div>
+                        </BootstrapTooltip>
+                    </div>
                 </div>
             </div>
         </div>
